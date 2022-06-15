a40467cb4caf2b74d2aa3992ed55f93d0e08ef82
Angelinho Diaz <20203tn127@utez.edu.mx>
2022-06-14 21:52:39 -0500

    Actualizacion minuto 55 parte 2

--- src/main/java/com/mx/edu/utez/strutsbasics/status/DaoStatus.java
+++ src/main/java/com/mx/edu/utez/strutsbasics/status/DaoStatus.java
@@ -1,2 +1,65 @@
-package com.mx.edu.utez.strutsbasics.status;public class DaoStatus {
+package com.mx.edu.utez.strutsbasics.status;
+
+import com.mx.edu.utez.strutsbasics.utils.ConnectionMySQL;
+import org.apache.struts2.components.Bean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DaoStatus {
+    Connection connection;
+    ResultSet rs;
+    PreparedStatement pstm;
+
+    public List<BeanStatus> findAllStatus() throws SQLException {
+        List<BeanStatus> listStatus = new ArrayList<>();
+        try{
+            connection = ConnectionMySQL.getConnection();
+            String script = "Select status.id, status.name from status";
+            pstm = connection.prepareStatement(script);
+            while (rs.next()){
+                BeanStatus status = new BeanStatus();
+                status.setId(rs.getInt("id"));
+                status.setName(rs.getString("name"));
+                listStatus.add(status);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }finally {
+            closeConnection();
+        }
+        return listStatus;
+    }
+
+    private void closeConnection() throws SQLException {
+        if (rs!=null){
+            rs.close();
+        }
+        if (pstm!=null){
+            pstm.close();
+        }
+        if (connection!=null){
+            connection.close();
+        }
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        DaoStatus dao = new DaoStatus();
+        for (BeanStatus status: dao.findAllStatus()){
+            System.out.println("Datos OG");
+            System.out.println(status.getId());
+            System.out.println(status.getName());
+        }
+    }
+
+
+
+
+
+
 }
--- src/main/webapp/js/modules/admin/admin.js
+++ src/main/webapp/js/modules/admin/admin.js
@@ -1,4 +1,21 @@
 
 app.controller('user', ['$scope', '$http', '$window', function ($scope, $http){
-    $scope.ola = "HOLA Angelinho do brasil"
+    $http.defaults.headers.post["content-type"] = "application/x-www-form-urlencoded";
+
+
+    $scope.arrayStatus = []
+
+    $scope.findAllStatus= () => {
+        $http({
+            method: 'POST',
+            url: 'Struts_Basics/findAllStatus'
+            }
+        ).then(function (response){
+            console.log(response)
+        })
+    }
+
+
+
+
 }])
